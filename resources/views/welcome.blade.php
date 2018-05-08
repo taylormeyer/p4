2ba9637280b6d766c70e7eec7fe4a9dec99d5d9c
Taylor Meyer <tam008@g.harvard.edu>
2018-05-08 01:29:09 -0400

    style change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="content landing-content">
+            <div class="content landing-content text-center">
                 <h2>Welcome to Taylor's Restaurant!</h2>
                     <p> Please login, or register to create an account. From there you can place your online order.</p>
             </div>
